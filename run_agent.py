446f320b40f5b04280bc6516899cdb9a3f06f8f5
Utkarsh Mishra <harshutkarshmishra1998@gmail.com>
2026-02-22 23:18:07 +0530

    Cleaning

--- run_agent.py
+++ run_agent.py
@@ -16,8 +16,8 @@ from evaluation_engine.langgraph_node import evaluation_node
 from tests.schema_mapping import extract_schema
 from tests.json_printer import print_last_n_role_constants
 
-METADATA_FILE = "uploaded_files/aids_virus_infection_prediction/metadata.json"
-DATA_FILE = "uploaded_files/aids_virus_infection_prediction/data.csv"
+METADATA_FILE = "uploaded_files/jigsaw_unintended_bias100k/metadata.json"
+DATA_FILE = "uploaded_files/jigsaw_unintended_bias100k/data.csv"
 
 cats, target = extract_schema(METADATA_FILE, DATA_FILE)
 
