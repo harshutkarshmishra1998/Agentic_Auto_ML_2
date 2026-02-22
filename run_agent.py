519cfeae0045a9ce3b8165a79e035a4f00c5da3f
Utkarsh Mishra <harshutkarshmishra1998@gmail.com>
2026-02-23 00:51:02 +0530

    New Dataset downloaded for testing

--- run_agent.py
+++ run_agent.py
@@ -17,8 +17,8 @@ from retrain.langgraph_node import retrain_node
 from tests.schema_mapping import extract_schema
 from tests.json_printer import print_last_n_role_constants
 
-METADATA_FILE = "uploaded_files/sloan-digital-sky-survey-dr16/metadata.json"
-DATA_FILE = "uploaded_files/sloan-digital-sky-survey-dr16/data.csv"
+METADATA_FILE = "uploaded_files/dataset_credit_score/metadata.jsonl"
+DATA_FILE = "uploaded_files/dataset_credit_score/data.csv"
 
 cats, target = extract_schema(METADATA_FILE, DATA_FILE)
 
