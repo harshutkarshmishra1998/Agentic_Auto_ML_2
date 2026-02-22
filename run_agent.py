09540ac66562b6e360051ceb6cf0f1d6f888e688
Utkarsh Mishra <harshutkarshmishra1998@gmail.com>
2026-02-23 01:10:10 +0530

    New data added

--- run_agent.py
+++ run_agent.py
@@ -17,8 +17,8 @@ from retrain.langgraph_node import retrain_node
 from tests.schema_mapping import extract_schema
 from tests.json_printer import print_last_n_role_constants
 
-METADATA_FILE = "uploaded_files/dataset_credit_score/metadata.jsonl"
-DATA_FILE = "uploaded_files/dataset_credit_score/data.csv"
+METADATA_FILE = "uploaded_files/absenteeism/metadata.jsonl"
+DATA_FILE = "uploaded_files/absenteeism/data.csv"
 
 cats, target = extract_schema(METADATA_FILE, DATA_FILE)
 
